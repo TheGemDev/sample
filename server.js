f30a957d7781dedfe5e444f9cc1a475f9cba2b38
Gabriel Momoh <53819287+TheGemDev@users.noreply.github.com>
2024-07-09 01:41:00 +0100

    pap

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const { PeerServer } = require("peer");
+const express = require("express");
+const app = express();
 const peerServer = PeerServer({
   port: process.env.PORT || 9000,
   path: "/myapp",
